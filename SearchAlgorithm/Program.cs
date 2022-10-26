256b64b63aa61c0a8e3f729d7a03b0e84f5bed3d
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-10-26 16:58:41 +0700

    add method linearsearch

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -81,5 +81,36 @@ namespace SearchAlgorithm
                 ch = char.Parse(Console.ReadLine().ToUpper());
             } while ((ch == 'y'));
         }
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                // Accept the number to be searched
+                Console.WriteLine("\nEnter the element you want to searh :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine
+                            ("\n" + item.ToString() + " found st position "
+                            + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString()
+                        + " not found in the array ");
+                Console.WriteLine("\n Number of comparison: " + ctr);
+                Console.Write("\nConsole search (y/n): ");
+                ch = Char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
+        }
     }
 }
