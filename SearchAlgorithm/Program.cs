6d9e6b7c4540527f806e880c31510e1299e9a12e
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-10-26 16:07:54 +0700

    variabel arr sebanyak 20, variabel n, variabel i

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace SearchAlgorithm
 {
-    internal class Program
+    class Program
     {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+
+        }
         static void Main(string[] args)
         {
         }
